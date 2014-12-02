cc7eea49031f8fc5841e69441227654b06d44268
SuangratW <apple@Mena-MacBook-Pro.local>
2014-12-02 15:20:18 +0700

    lovemac commit1

---
+++ lovemac.rb
@@ -0,0 +1,13 @@
+def lovemac(number)
+  # Put your code in here  
+ if(number%3==0 && number%5!=0)
+ 	puts "Love"
+ elsif(number%5==0 && number%3!=0) 
+ 	puts "Mac"
+ elsif(number%5==0 && number%3==0)
+  	puts "HateWindows"
+ else puts number
+
+ end
+
+end
